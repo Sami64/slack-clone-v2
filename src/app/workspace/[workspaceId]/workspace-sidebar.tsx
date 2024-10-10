bde43b56c8bbd8cf1e22e4b1ed653be9f827df73
Samuel Kow Paintsil <smashbros54@gmail.com>
2024-10-10 23:42:12 +0000

    ⚡️ show channels

--- src/app/workspace/[workspaceId]/workspace-sidebar.tsx
+++ src/app/workspace/[workspaceId]/workspace-sidebar.tsx
@@ -1,8 +1,17 @@
+import { useGetChannels } from '@/features/channels/api/use-get-channels'
 import { useCurrentMember } from '@/features/members/api/use-current-member'
 import { useGetWorkspace } from '@/features/workspaces/api/use-get-workspace'
 import { useWorkspaceId } from '@/hooks/use-workspace-id'
-import { AlertTriangle, Loader } from 'lucide-react'
+import {
+	AlertTriangle,
+	HashIcon,
+	Loader,
+	MessageSquareText,
+	SendHorizonal,
+} from 'lucide-react'
+import SidebarItem from './sidebar-item'
 import WorkspaceHeader from './workspace-header'
+import WorkspaceSection from './workspace-section'
 
 const WorkspaceSidebar = () => {
 	const workspaceId = useWorkspaceId()
@@ -11,6 +20,9 @@ const WorkspaceSidebar = () => {
 	})
 	const { data: workspace, isLoading: workspaceLoading } = useGetWorkspace({
 		id: workspaceId,
+	})
+	const { data: channels, isLoading: channelsLoading } = useGetChannels({
+		workspaceId,
 	})
 
 	if (workspaceLoading || memberLoading) {
@@ -36,6 +48,31 @@ const WorkspaceSidebar = () => {
 				workspace={workspace}
 				isAdmin={member.role === 'admin'}
 			/>
+			<div className="flex flex-col px-2 mt-3">
+				<SidebarItem
+					label="Threads"
+					icon={MessageSquareText}
+					id="threads"
+				/>
+				<SidebarItem
+					label="Drafts & Sent"
+					icon={SendHorizonal}
+					id="draft"
+				/>
+			</div>
+			<WorkspaceSection
+				label="Channels"
+				hint="New Channel"
+				onNew={() => {}}>
+				{channels?.map((item) => (
+					<SidebarItem
+						key={item._id}
+						id={item._id}
+						label={item.name}
+						icon={HashIcon}
+					/>
+				))}
+			</WorkspaceSection>
 		</div>
 	)
 }
